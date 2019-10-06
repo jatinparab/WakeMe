d58635bc10695b9131c833b8e26f956b6701d824
Unknown <owaiswizard@gmail.com>
2019-10-06 20:41:17 +0530

    Add EyeBlinkDetector

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -14,6 +14,7 @@ import { MonoText } from '../components/StyledText';
 import HeartChart  from '../components/charts/HeartChart';
 import SkinChart  from '../components/charts/SkinChart';
 import LiveStream from '../components/streamer/LiveStream';
+import EyeBlinkDetector from '../components/EyeBlinkDetector'
 
 export default function HomeScreen() {
   return (
@@ -23,7 +24,7 @@ export default function HomeScreen() {
       <SkinChart></SkinChart>
       </View>
       <View style={styles.bottomContainer}>
-      <LiveStream/>
+        <EyeBlinkDetector />
       </View>
     </View>
   );
